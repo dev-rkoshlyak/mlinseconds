a576df25bd94895a328beb78edde9bfc294a9160
Roman Koshlyak <roman@troider.com>
2018-12-23 11:41:11 -0800

    vote prediction

---
+++ problems/votePrediction.py
@@ -0,0 +1,142 @@
+# There are random function from 8 inputs.
+# There are random input vector of size 8 * number of voters.
+# We calculate function number of voters times and sum result.
+# We return 1 if sum > voters/2, 0 otherwise
+# We split data in 2 parts, on first part you will train and on second
+# part we will test
+import time
+import random
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import sys
+sys.path.append('./../utils')
+import solutionmanager as sm
+
+class SolutionModel(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(SolutionModel, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = 32
+        self.linear1 = nn.Linear(input_size, self.hidden_size)
+        self.linear2 = nn.Linear(self.hidden_size, self.hidden_size)
+        self.linear3 = nn.Linear(self.hidden_size, output_size)
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = F.relu(x)
+        x = self.linear2(x)
+        x = F.relu(x)
+        x = self.linear3(x)
+        x = F.sigmoid(x)
+        return x
+
+class Solution():
+    def __init__(self):
+        self = self
+
+    def create_model(self, input_size, output_size):
+        return SolutionModel(input_size, output_size)
+
+    # Return number of steps used
+    def train_model(self, model, train_data, train_target, context):
+        step = 0
+        # Put model in train mode
+        model.train()
+        while True:
+            time_left = context.get_timer().get_time_left()
+            # No more time left, stop training
+            if time_left < 0.1:
+                break
+            optimizer = optim.SGD(model.parameters(), lr=1)
+            data = train_data
+            target = train_target
+            # model.parameters()...gradient set to zero
+            optimizer.zero_grad()
+            # evaluate model => model.forward(data)
+            output = model(data)
+            # if x < 0.5 predict 0 else predict 1
+            predict = output.round()
+            # Number of correct predictions
+            correct = predict.eq(target.view_as(predict)).long().sum().item()
+            # Total number of needed predictions
+            total = target.view(-1).size(0)
+            # calculate loss
+            bce_loss = nn.BCELoss()
+            loss = bce_loss(output, target)
+            # calculate deriviative of model.forward() and put it in model.parameters()...gradient
+            loss.backward()
+            # print progress of the learning
+            self.print_stats(step, loss, correct, total)
+            # update model: model.parameters() -= lr * gradient
+            optimizer.step()
+            step += 1
+        return step
+    
+    def print_stats(self, step, loss, correct, total):
+        if step % 100 == 0:
+            print("Step = {} Prediction = {}/{} Error = {}".format(step, correct, total, loss.item()))
+
+###
+###
+### Don't change code after this line
+###
+###
+class Limits:
+    def __init__(self):
+        self.time_limit = 2.0
+        self.size_limit = 1000000
+        self.test_limit = 1.0
+
+class DataProvider:
+    def __init__(self):
+        self.number_of_cases = 10
+
+    def get_index(self, tensor_index):
+        index = 0
+        for i in range(tensor_index.size(0)):
+            index = 2*index + tensor_index[i].item()
+        return index
+
+    def calc_value(self, input_data, function_table, input_size, input_count_size):
+        count = 0
+        for i in range(input_count_size):
+            count += function_table[self.get_index(input_data[i*input_size: (i+1)*input_size])].item()
+        if count > input_count_size/2:
+            return 1
+        else:
+            return 0
+
+    def create_data(self, data_size, input_size, input_count_size, seed):
+        torch.manual_seed(seed)
+        function_size = 1 << input_size
+        function_table = torch.ByteTensor(function_size).random_(0, 2)
+        total_input_size = input_size*input_count_size
+        data = torch.ByteTensor(data_size, total_input_size).random_(0, 2)
+        target = torch.ByteTensor(data_size)
+        for i in range(data_size):
+            target[i] = self.calc_value(data[i], function_table, input_size, input_count_size)
+        return (data.float(), target.view(-1, 1).float())
+
+    def create_case_data(self, case):
+        input_size = 8
+        data_size = (1<<input_size)*32
+        input_count_size = case
+
+        data, target = self.create_data(2*data_size, input_size, input_count_size, case)
+        return sm.CaseData(case, Limits(), (data[:data_size], target[:data_size]), (data[data_size:], target[data_size:])).set_description("{} inputs per voter and {} voters".format(input_size, input_count_size))
+
+class Config:
+    def __init__(self):
+        self.max_samples = 10000
+
+    def get_data_provider(self):
+        return DataProvider()
+
+    def get_solution(self):
+        return Solution()
+
+# If you want to run specific case, put number here
+sm.SolutionManager(Config()).run(case_number=-1)
