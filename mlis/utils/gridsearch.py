2cf418fee48b5dcbd8ee6bb9ec875af5f2c44e63
Roman Koshlyak <rkoshlyak@gmail.com>
2019-06-25 16:43:58 +0300

    Command line gridsearch
    1. Create ability to specify grid search via command line
    2. Save/load results data to/from file
    3. Change results data filter to dataframe query format

--- mlis/utils/gridsearch.py
+++ mlis/utils/gridsearch.py
@@ -1,4 +1,7 @@
 import random
+import pickle
+import os
+import sys
 import torch
 import inspect
 import collections
@@ -6,13 +9,6 @@ from . import solutionmanager as sm
 from . import speedtest
 
 class ResultsData():
-    @staticmethod
-    def get_global():
-        global RESULTS_DATA_INSTANCE
-        if 'RESULTS_DATA_INSTANCE' not in globals():
-            RESULTS_DATA_INSTANCE = ResultsData()
-        return RESULTS_DATA_INSTANCE
-
     def __init__(self):
         self.clear_data()
 
@@ -44,7 +40,26 @@ class ResultsData():
     def get_results(self, choice_str, name):
         return list(self.results_values[choice_str][name].values())
 
+    def save(self, file_name):
+        file_pi = open(file_name, 'wb')
+        pickle.dump(self, file_pi)
+
+    @staticmethod
+    def load(file_name):
+        if os.path.isfile(file_name):
+            return pickle.load(open(file_name, 'rb'))
+        return ResultsData()
+
+    @staticmethod
+    def get_global():
+        global RESULTS_DATA_INSTANCE
+        if 'RESULTS_DATA_INSTANCE' not in globals():
+            RESULTS_DATA_INSTANCE = ResultsData()
+        return RESULTS_DATA_INSTANCE
+
+
 class GridSearch():
+    ITER_NUMBER = 'iter_number'
     GRID_LIST_SUFFIX = '_grid'
     GRID_PARAM_SEPARATOR = ' '
     GRID_VALUE_SEPARATOR = ':'
@@ -52,7 +67,27 @@ class GridSearch():
     def __init__(self):
         self.results_data = None
 
+    def get_grid_attributes_from_parser(self, parser):
+        all_args = ';'.join(sys.argv)
+        iter_number = 1
+        args = parser.parse_args()
+        grid_attributes = collections.OrderedDict()
+        sorted_attributes = []
+        for key, value in vars(args).items():
+            if key == GridSearch.ITER_NUMBER:
+                iter_number = value
+            else:
+                index = all_args.find(key)
+                if index == -1:
+                    grid_attributes[key] = value
+                else:
+                    sorted_attributes.append((index, key, value))
+        for _, key, value in sorted(sorted_attributes):
+            grid_attributes[key] = value
+        return iter_number, grid_attributes
+
     def get_grid_attributes(self, solution):
+        iter_number = self.get_iter_number(solution)
         s = solution
         code = inspect.getsource(type(s))
         grid_attrs =[(code.index(a), a) for a in dir(s) if self.filter_grid_attribute(s, a)]
@@ -61,7 +96,7 @@ class GridSearch():
         grid_attributes = collections.OrderedDict()
         for _, a in grid_attrs:
             grid_attributes[self.get_grid_attribute(s, a)] = self.get_grid_attribute_list(s, a)
-        return grid_attributes
+        return iter_number, grid_attributes
 
     def get_grid_attribute(self, obj, attr):
         return attr[:-len(GridSearch.GRID_LIST_SUFFIX)]
@@ -121,12 +156,17 @@ class GridSearch():
         return t.mean().item(), t.std().item()
 
     def get_iter_number(self, solution):
-        if hasattr(solution, 'iter_number'):
+        if hasattr(solution, GridSearch.ITER_NUMBER):
             return solution.iter_number
         else:
             return 1
 
-    def run(self, config, case_number, random_order=False, verbose=False, results_data = None):
+    def check_grid_attributes(self, grid_attributes):
+        for key, value in grid_attributes.items():
+            unique_len = len(set([str(x) for x in value]))
+            if len(value) != unique_len:
+                raise ValueError('Non unique attributes: {} = {}'.format(key, value))
+    def run(self, config, case_number, random_order=False, verbose=False, results_data = None, grid_parser = None):
         speed_calculator = speedtest.SpeedCalculator()
         time_mult = speed_calculator.calc_linear_time_mult()
         print("Local CPU time mult = {:.2f}".format(time_mult))
@@ -135,10 +175,14 @@ class GridSearch():
         if results_data is None:
             results_data = ResultsData()
         self.results_data = results_data
-        grid_attributes = self.get_grid_attributes(solution)
+        if grid_parser is None:
+            iter_number, grid_attributes = self.get_grid_attributes(solution)
+        else:
+            iter_number, grid_attributes = self.get_grid_attributes_from_parser(grid_parser)
+            solution.iter_number = iter_number
+        self.check_grid_attributes(grid_attributes)
         self.results_data.set_grid_attributes(grid_attributes)
         grid_size = self.calc_grid_size(grid_attributes)
-        iter_number = self.get_iter_number(solution)
         if verbose:
             print('[Grid search] Runing: grid_size={} iter_number={} verbose={}'.format(grid_size, iter_number, verbose))
         grid_choice_history = {}
@@ -153,7 +197,7 @@ class GridSearch():
                     print('[Grid search] Running: choise_str={} iter={}'.format(choice_str, iter))
                 solution.iter = iter
                 self.init_seed = results_data.get_next_init_seed(choice_str)
-                solution_manager.train_model(self.init_seed, solution, case_data)
+                solution_manager.train_model(self.init_seed, solution, case_data, time_mult)
 
             grid_choice_history[choice_str] = True
         print(solution_manager.accepted_string("[SEARCH COMPLETED]"))
--- mlis/utils/plotter.py
+++ mlis/utils/plotter.py
@@ -1,33 +1,6 @@
 import pandas as pd
 import seaborn as sns
 
-class Filter():
-    def __init__(self, columns, filt):
-        self.filt = self.build_filter(columns, filt)
-
-    def build_filter(self, columns, filt):
-        result = []
-        if filt is not None:
-            column_to_id = {column: ind for ind, column in enumerate(columns)}
-            for key, value in filt.items():
-                if type(key) is str:
-                    key = column_to_id[key]
-                if type(value) not in [tuple, list]:
-                    value = [value]
-                result.append((key, value))
-        return result
-
-    def is_good(self, value, value_list):
-        if type(value_list) is tuple:
-            return value_list[0] <= value and value <= value_list[1]
-        return value in value_list
-
-    def __call__(self, row):
-        for key, value in self.filt:
-            if not self.is_good(row[key], value):
-                return False
-        return True
-
 class Plotter():
     COLUMN_DELIMITER = ':'
     def __init__(self, results_data):
@@ -47,7 +20,7 @@ class Plotter():
                     columns_dict[column] = True
         return columns
 
-    def get_dataframe(self, filt):
+    def get_dataframe(self):
         columns_grid = self.get_columns()
         value_types = self.get_value_types()
         columns = columns_grid + ['type', 'value']
@@ -56,23 +29,26 @@ class Plotter():
         for key, values in self.results_data.results_values.items():
             choices = self.results_data.results_choices[key]
             datum = [choices[column] for column in columns_grid]
-            if filt(datum):
-                for value_type in value_types:
-                    for _, value in values[value_type].items():
-                        datum_all = datum.copy()
-                        datum_all.append(value_type)
-                        datum_all.append(value)
-                        data.append(datum_all)
+            for value_type in value_types:
+                for _, value in values[value_type].items():
+                    datum_all = datum.copy()
+                    datum_all.append(value_type)
+                    datum_all.append(value)
+                    data.append(datum_all)
 
         df = pd.DataFrame(data, columns=columns)
         return df
 
-    def show_1d(self, column_index = -1, filt = None):
+    def show_1d(self, column_index = -1, query = None):
         columns = self.get_columns()
-        filt = Filter(columns, filt)
         x_column = columns.pop(column_index)
-        df = self.get_dataframe(filt)
-        col_column = Plotter.COLUMN_DELIMITER.join(columns)
+        df = self.get_dataframe()
+        if query is not None:
+            df.query(query, inplace=True)
+        if df.shape[0] == 0:
+            print('[WARNING] Empty filtered results')
+            return
+        col_column = 'col:'+Plotter.COLUMN_DELIMITER.join(columns)
         df[col_column] = df.apply (lambda row: Plotter.COLUMN_DELIMITER.join([str(row[c]) for c in columns]), axis=1)
         sns.set(style="darkgrid")
         sns.relplot(x=x_column, y="value", hue="type", col=col_column, col_wrap=2, kind="line", markers=True, data=df)
